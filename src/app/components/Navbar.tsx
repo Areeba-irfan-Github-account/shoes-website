e7fdba17f0d634b898c0b20cdcb459938c4c65ae
Areeba Irfan <161799302+Areeba-irfan-Github-account@users.noreply.github.com>
2024-12-10 22:29:53 +0500

    Updated🎇

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,6 +1,7 @@
+'use client'
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 import { BiShoppingBag } from "react-icons/bi";
 import { CiSearch, } from "react-icons/ci";
 import { FaRegHeart } from "react-icons/fa";
@@ -8,8 +9,11 @@ import { FaRegHeart } from "react-icons/fa";
 
 
 const Navbar = () => {
+    const [isHamburgerOpen, setHamburgerOpen] = useState(false);
+    const toggleHamburger = () => setHamburgerOpen(!isHamburgerOpen);
+
     return (
-        <div className="bg-main w-full flex flex-row items-center justify-between py-0 px-6 shadow-lg">
+        <div className="bg-main max-w-[1440px] mx-auto h-full flex items-center justify-between px-6 py-0 shadow-lg">
             {/* Logo */}
             <div className="flex-shrink-0">
                 <Link href='/'>
@@ -21,57 +25,105 @@ const Navbar = () => {
                     />
                 </Link>
             </div>
-            {/* Navigation Links */}
-            <div className="flex-1 flex justify-center md:ml-20 space-x-6 text-sm md:text-base font-medium text-black">
-                <Link href="/" className="hover:text-gray-900 hidden lg:block">
-                    News & Featured
-                </Link>
-                <Link href="/men" className="hover:text-gray-900">
-                    Men
-                </Link>
-                <Link href="/products" className="hover:text-gray-900">
-                    Women
-                </Link>
-                <Link href="/products" className="hover:text-gray-900">
-                    Kids
-                </Link>
-                <Link href="/products" className="hover:text-gray-900">
-                    Sale
-                </Link>
-                <Link href="/" className="hover:text-gray-900">
-                    SNKRS
-                </Link>
-            </div>
+            <div className='block lg:hidden'>
 
-            {/* Search, Heart Icon, and Cart Icon */}
+                <div className="absolute top-12 right-2">
+                    <button onClick={toggleHamburger} className="text-2xl">
+                        {isHamburgerOpen ? "X" : "☰"}
+                    </button>
+                </div>
+                {/* Mobile Menu */}
+                {isHamburgerOpen && (
+                    <div className="absolute  right-0 mt-11 w-48 text-white bg-black/60 backdrop-blur-md rounded-md shadow-lg m-3 p-4">
+                        <div className="flex flex-col space-y-2">
+                            <Link href="/" className="hover:text-gray-900 hidden lg:block">
+                                News & Featured
+                            </Link>
+                            <Link href="/men" className="hover:text-gray-900">
+                                Men
+                            </Link>
+                            <Link href="/products" className="hover:text-gray-900">
+                                Women
+                            </Link>
+                            <Link href="/products" className="hover:text-gray-900">
+                                Kids
+                            </Link>
+                            <Link href="/products" className="hover:text-gray-900">
+                                Sale
+                            </Link>
+                            <Link href="/" className="hover:text-gray-900">
+                                SNKRS
+                            </Link>
+                            <Link href="/products" className=" hover:text-gray-600 text-white text-sm md:text-base">
+                                Find a Store 
+                            </Link>
+                            <Link href="/help" className="hover:text-gray-600 text-sm md:text-base">
+                                Help 
+                            </Link>
+                            <Link href="/joinus" className="hover:text-gray-600 text-sm md:text-base">
+                                Join Us 
+                            </Link>
+                            <Link href="/signin" className="hover:text-gray-600 text-sm md:text-base">
+                                Sign In
+                            </Link>
+                        </div>
+                    </div>
+                )}
+            </div>
+            {/* Navigation Links */}
             <div className='lg:block hidden'>
-                <div className="flex items-center space-x-4">
-                    {/* Search Input */}
-                    <div className="flex items-center bg-secondary rounded-full pl-4 py-3 focus-within:ring-2 focus-within:ring-blue-400">
-                        {/* Search Icon */}
-                        <CiSearch className="mr-2 text-black text-xl" /> {/* Ensure the icon is visible with text-xl */}
-                        <input
-                            type="text"
-                            placeholder="Search"
-                            className="flex-1 text-sm focus:outline-none bg-transparent rounded-full placeholder-gray-500"
-                        />
+                <div className='flex flex-row justify-between space-x-32 items-center'>
+
+                    <div className="flex-1 flex justify-center md:ml-20 space-x-6 text-sm md:text-base font-medium text-black">
+                        <Link href="/" className="hover:text-gray-900 hidden lg:block">
+                            News & Featured
+                        </Link>
+                        <Link href="/men" className="hover:text-gray-900">
+                            Men
+                        </Link>
+                        <Link href="/products" className="hover:text-gray-900">
+                            Women
+                        </Link>
+                        <Link href="/products" className="hover:text-gray-900">
+                            Kids
+                        </Link>
+                        <Link href="/products" className="hover:text-gray-900">
+                            Sale
+                        </Link>
+                        <Link href="/" className="hover:text-gray-900">
+                            SNKRS
+                        </Link>
                     </div>
 
-                    {/* Heart Icon */}
-                    <button className="text-gray-700 hover:text-gray-900 focus:outline-none">
-                        <FaRegHeart size={20} />
-                    </button>
+                    {/* Search, Heart Icon, and Cart Icon */}
+                    <div className="flex items-center space-x-4">
+                        {/* Search Input */}
+                        <div className="flex items-center bg-secondary rounded-full pl-4 py-3 focus-within:ring-2 focus-within:ring-blue-400">
+                            {/* Search Icon */}
+                            <CiSearch className="mr-2 text-black text-xl" /> {/* Ensure the icon is visible with text-xl */}
+                            <input
+                                type="text"
+                                placeholder="Search"
+                                className="flex-1 text-sm focus:outline-none bg-transparent rounded-full placeholder-gray-500"
+                            />
+                        </div>
 
-                    {/* Cart Icon */}
-                    <Link href='/checkout' className="text-gray-700 hover:text-gray-900 focus:outline-none" >
-                        <BiShoppingBag size={20} />
-                    </Link>
+                        {/* Heart Icon */}
+                        <button className="text-gray-700 hover:text-gray-900 focus:outline-none">
+                            <FaRegHeart size={20} />
+                        </button>
+
+                        {/* Cart Icon */}
+                        <Link href='/checkout' className="text-gray-700 hover:text-gray-900 focus:outline-none" >
+                            <BiShoppingBag size={20} />
+                        </Link>
+                    </div>
                 </div>
+
+
             </div>
 
-
         </div>
-
     );
 };
 
